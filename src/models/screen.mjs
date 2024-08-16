f445d1ff9aeb2f67cc99feb3aebfbaa155fab2d4
YuriSS <yuri.santos@db.tec.br>
2024-08-15 23:05:12 -0300

    feat: screen initialized

---
+++ src/models/screen.mjs
@@ -0,0 +1,19 @@
+import { Vector } from './vector.mjs';
+
+export class Screen {
+  #factor; #width; #height;
+
+  constructor(factor) {
+    this.#factor = 
+      (factor?.constructor !== Number || factor <= 30 || factor > 60)
+        ? 30
+        : factor;
+
+    this.#width = 16 * this.#factor;
+    this.#height = 9 * this.#factor;
+  }
+
+  values = () => [this.#width, this.#height];
+  plainObject = () => ({ width: this.#width, height: this.#height });
+  vector = () => new Vector(this.#width, this.#height);
+}
