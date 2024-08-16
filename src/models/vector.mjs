77308673f0be02c314c081783b39df25d7b1ec8b
YuriSS <yuri.santos@db.tec.br>
2024-08-15 21:42:26 -0300

    feat: added vector class

---
+++ src/models/vector.mjs
@@ -0,0 +1,39 @@
+export class Vector {
+  #x; #y;
+
+  constructor(x, y) {
+    if (x.constructor !== Number || y.constructor !== Number) {
+      throw new Error('Invalid arguments');
+    }
+
+    this.#x = x;
+    this.#y = y;
+  }
+
+  x = () => this.#x;
+  y = () => this.#y;
+  values = () => [this.#x, this.#y];
+  plainObject = () => ({ x: this.#x, y: this.#y });
+
+  add = (vector) => {
+    this.#x += vector.x();
+    this.#y += vector.y();
+  };
+
+  subtract = (vector) => {
+    this.#x -= vector.x();
+    this.#y -= vector.y();
+  };
+
+  multiply = (scalar) => {
+    this.#x *= scalar;
+    this.#y *= scalar;
+  };
+
+  divide = (scalar) => {
+    this.#x /= scalar;
+    this.#y /= scalar;
+  };
+
+  magnitude = () => Math.sqrt(this.#x ** 2 + this.#y ** 2);
+}
