484db224bafd5c0af05514824e574e97adc6ccc7
YuriSS <yuri.santos@db.tec.br>
2024-08-16 10:48:50 -0300

    feat: created a randon generation line graph

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,11 @@
+import { GeneratedData } from "./data-set.mjs";
+import { logScopeEnum } from "./infra/log.mjs";
 import { State } from "./models/state.mjs";
+import { BiVector } from "./models/vector.mjs";
+import { Screen } from "./models/screen.mjs";
 
-const state = new State("graph");
+const state = new State("graph", { screen: new Screen(200) });
+let running = true;
 
 document.body.style.backgroundColor = state.getTheme().getBackgroundColor();
 document.body.style.color = state.getTheme().getForegroundColor();
@@ -8,9 +13,38 @@ document.body.style.color = state.getTheme().getForegroundColor();
 state.getCanvas().width = state.getScreen().values()[0];
 state.getCanvas().height = state.getScreen().values()[1];
 
-state.ctx().beginPath();
-state.ctx().moveTo(0, 0);
-state.ctx().lineTo(100, 100);
-state.ctx().strokeStyle = state.getTheme().getForegroundColor();
-state.ctx().stroke();
-state.ctx().closePath();
+const dataGenerator = new GeneratedData();
+const mapDataSetToBiVector = (dataSet) =>
+  dataSet.map((data) => new BiVector(...data));
+
+function draw() {
+  const data = mapDataSetToBiVector(
+    dataGenerator.generate(1000, 100).getData()
+  );
+
+  state.ctx().clearRect(0, 0, state.getScreen().vector().x(), state.getScreen().vector().y());
+  data.reduce((previous, current) => {
+    state.log(
+      logScopeEnum.INFO,
+      `Drawing line from ${previous.values().join(', ')} to ${current.values().join(', ')}`
+    );
+
+    state.ctx().beginPath();
+    state.ctx().moveTo(...previous.values());
+    state.ctx().lineTo(...current.values());
+    state.ctx().strokeStyle = state.getTheme().getForegroundColor();
+    state.ctx().stroke();
+    state.ctx().closePath();
+
+    return current;
+  });
+
+  if (running) window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
+
+document.getElementById("stop").addEventListener("click", () => {
+  running = !running;
+  if (running) window.requestAnimationFrame(draw);
+});
