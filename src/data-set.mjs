de26a63f3dc28af1bc126481d1e48865b1f14fd2
YuriSS <yuri.santos@db.tec.br>
2024-08-16 14:31:19 -0300

    feat: changed parameters o randon generate data and added log

--- src/data-set.mjs
+++ src/data-set.mjs
@@ -1,3 +1,5 @@
+import { LogBuilder } from "./infra/log/index.mjs";
+
 export const simpleDataSet = [
   [1, 2],
   [2, 3],
@@ -12,11 +14,21 @@ export const simpleDataSet = [
 ];
 
 export class GeneratedData {
-  #data = [[0,0]]; #MAX_SIZE = 100; #GAP = 30;
+  #data = [[0,0]]; #MAX_SIZE = 100; #GAP = 30; #seed = 100;
+  #appLog = new LogBuilder();
 
   getData = () => this.#data;
-  generate = (quantity, seed) => {
-    const s = Math.min(quantity, this.#MAX_SIZE);
+  generate = (seed = this.#seed, quantity = this.#MAX_SIZE, gap = this.#GAP) => {
+    if (seed?.constructor !== Number
+          || quantity?.constructor !== Number
+          || gap?.constructor !== Number
+    ) {
+      throw new Error("[Generate randon data] Invalid arguments");
+    }
+
+    this.#appLog.only(`Generating data with seed: ${seed}, quantity: ${quantity}, gap: ${gap}`);
+
+    const s = Math.min(quantity, gap);
     this.#data = new Array(s).fill(0).map((_, i) => {
       const n = Math.floor(Math.random() * seed);
       return [
