ce7c38ad802db17d3940d8a10c15c4ba68e618d3
YuriSS <yuri.santos@db.tec.br>
2024-08-17 01:33:42 -0300

    feat: added grid function

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,8 @@
 import { State } from "./models/state/index.mjs";
 import { Screen } from "./models/screen/index.mjs";
 import { BiVector } from "./models/vector/index.mjs";
-import { Clock } from "./components/clock/index.mjs";
-import { LogBuilder } from "./infra/log/index.mjs";
 
-const state = new State("graph", { screen: new Screen(60) });
+const state = new State("graph", { screen: new Screen(100) });
 
 document.body.style.textAlign = 'center';
 document.body.style.backgroundColor = state.getTheme().getBackgroundColor();
@@ -14,17 +12,30 @@ state.getCanvas().width = state.getScreen().values()[0];
 state.getCanvas().height = state.getScreen().values()[1];
 
 normalizeOrigin(state);
+drawCoordinates(state);
+grid(state, 100);
 
-function draw() {
-  state.ctx().clearRect(
-    -state.getScreen().vector().x() / 2,
-    -state.getScreen().vector().y() / 2, state.getScreen().vector().x(), state.getScreen().vector().y()
-  );
-  new Clock(state).draw();
-  requestAnimationFrame(draw);
+function grid(state, squareQuantity = 10) {
+  if (state?.constructor !== State || squareQuantity?.constructor !== Number) {
+    throw new Error("[Grid] Invalid arguments");
+  }
+
+  const screenSize = state.getScreen().vector();
+  const gapX = (screenSize.x() - 2) / squareQuantity;
+  const gapY = (screenSize.y() - 2) / squareQuantity;
+  state.ctx().beginPath();
+  state.ctx().strokeStyle = state.getTheme().getForegroundColor().concat('10');
+  for (let x = -screenSize.x() + 2; x < screenSize.x(); x += gapX) {
+    state.ctx().moveTo(x, -screenSize.y());
+    state.ctx().lineTo(x, screenSize.y());
+  }
+  for (let y = -screenSize.y() + 2; y < screenSize.y(); y += gapY) {
+    state.ctx().moveTo(-screenSize.x(), y);
+    state.ctx().lineTo(screenSize.x(), y);
+  }
+  state.ctx().stroke();
+  state.ctx().closePath();
 }
-
-requestAnimationFrame(draw);
 
 function normalizeOrigin(state) {
   if (state?.constructor !== State) {
@@ -48,7 +59,7 @@ function drawCoordinates(state) {
   state.ctx().lineTo(0, state.getScreen().vector().y());
   state.ctx().moveTo(state.getScreen().vector().x() * -1, 0);
   state.ctx().lineTo(state.getScreen().vector().x(), 0);
-  state.ctx().strokeStyle = state.getTheme().getForegroundColor();
+  state.ctx().strokeStyle = state.getTheme().getForegroundColor().concat('10');
   state.ctx().stroke();
   state.ctx().closePath();
 }
